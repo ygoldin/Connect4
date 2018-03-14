47629c0c19ff9f019432bf1cdbba77580e13c159
ygold <ygold@173.250.190.246>
2018-03-13 22:34:53 -0700

    colors showing up on grid

--- src/Connect4Frame.java
+++ src/Connect4Frame.java
@@ -1,16 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 
 public class Connect4Frame extends JFrame {
 	private Connect4Model connect4Model;
 	private ColumnButton[] columns;
+	private static final Color[] PLAYER_COLORS = {Color.RED, Color.YELLOW};
 	
 	public Connect4Frame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setMinimumSize(new Dimension(1024, 768));
 		setTitle("Connect 4");
-		setBackground(Color.BLUE);
 		connect4Model = new Connect4Model();
 		setupButtons();
 	}
@@ -36,14 +35,15 @@ public class Connect4Frame extends JFrame {
 			rows = new JLabel[Connect4Model.ROWS];
 			this.setLayout(new GridLayout(rows.length, 1));
 			for(int r = 0; r < rows.length; r++) {
-				rows[r] = new JLabel("" + r + columnNumber);
+				rows[r] = new JLabel();
 				this.add(rows[r]);
 			}
 			addActionListener(e -> {
 				if(!connect4Model.isGameOver() && !connect4Model.columnFilled(columnNumber)) {
 					int curPlayer = connect4Model.getCurPlayer();
 					int placedRow = connect4Model.putDisk(columnNumber);
-					rows[placedRow].setText("player" + curPlayer);
+					rows[placedRow].setBackground(PLAYER_COLORS[curPlayer - 1]);
+					rows[placedRow].setOpaque(true);
 				}
 			});
 		}
