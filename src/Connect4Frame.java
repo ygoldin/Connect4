dfd968205c07eb19fb7ad2fd3e61430351dc23d5
ygold <ygold@173.250.190.246>
2018-03-13 17:51:26 -0700

    button made for every column of the grid

--- src/Connect4Frame.java
+++ src/Connect4Frame.java
@@ -3,7 +3,7 @@ import java.awt.*;
 
 public class Connect4Frame extends JFrame {
 	private Connect4Model model;
-	private JButton[][] buttons;
+	private ColumnButton[] columns;
 	
 	public Connect4Frame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -11,19 +11,33 @@ public class Connect4Frame extends JFrame {
 		setTitle("Connect 4");
 		setBackground(Color.BLUE);
 		model = new Connect4Model();
+		//setupButtons();
 		setupButtons();
 	}
 	
 	private void setupButtons() {
 		JPanel buttonPanel = new JPanel();
-		buttonPanel.setLayout(new GridLayout(Connect4Model.ROWS, Connect4Model.COLUMNS));
-		buttons = new JButton[Connect4Model.ROWS][Connect4Model.COLUMNS];
-		for(int r = 0; r < buttons.length; r++) {
-			for(int c = 0; c < buttons[r].length; c++) {
-				buttons[r][c] = new JButton("" + r + c);
-				buttonPanel.add(buttons[r][c]);
-			}
+		buttonPanel.setLayout(new GridLayout(1, Connect4Model.COLUMNS));
+		columns = new ColumnButton[Connect4Model.COLUMNS];
+		for(int c = 0; c < columns.length; c++) {
+			columns[c] = new ColumnButton(c);
+			buttonPanel.add(columns[c]);
 		}
 		add(buttonPanel);
+	}
+	
+	private static class ColumnButton extends JButton {
+		public JLabel[] rows;
+		public int columnNumber;
+		
+		public ColumnButton(int column) {
+			columnNumber = column;
+			rows = new JLabel[Connect4Model.ROWS];
+			this.setLayout(new GridLayout(rows.length, 1));
+			for(int r = 0; r < rows.length; r++) {
+				rows[r] = new JLabel("" + r + columnNumber);
+				this.add(rows[r]);
+			}
+		}
 	}
 }
