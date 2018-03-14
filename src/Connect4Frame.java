629537fd7b2cf0f157a89e97fc0da55d879594d8
ygold <ygold@173.250.190.246>
2018-03-13 17:39:36 -0700

    initial attempt to set up 6x7 grid of buttons

--- src/Connect4Frame.java
+++ src/Connect4Frame.java
@@ -3,11 +3,27 @@ import java.awt.*;
 
 public class Connect4Frame extends JFrame {
 	private Connect4Model model;
+	private JButton[][] buttons;
 	
 	public Connect4Frame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setMinimumSize(new Dimension(1024, 768));
 		setTitle("Connect 4");
+		setBackground(Color.BLUE);
 		model = new Connect4Model();
+		setupButtons();
+	}
+	
+	private void setupButtons() {
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridLayout(Connect4Model.ROWS, Connect4Model.COLUMNS));
+		buttons = new JButton[Connect4Model.ROWS][Connect4Model.COLUMNS];
+		for(int r = 0; r < buttons.length; r++) {
+			for(int c = 0; c < buttons[r].length; c++) {
+				buttons[r][c] = new JButton("" + r + c);
+				buttonPanel.add(buttons[r][c]);
+			}
+		}
+		add(buttonPanel);
 	}
 }
